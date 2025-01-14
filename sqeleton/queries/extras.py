f486f93b934dafe84ce0882351c126816867c4e4
Erez Shinan <erezshin+git@gmail.com>
2025-01-14 10:25:50 +0100

    Fix for runtype>0.5.0 (qualified name on dispatch)

--- sqeleton/queries/extras.py
+++ sqeleton/queries/extras.py
@@ -28,42 +28,44 @@ class ApplyFuncAndNormalizeAsString(ExprNode):
 class Checksum(ExprNode):
     exprs: Sequence[Expr]
 
-@md
-def compile_node(c: Compiler, n: NormalizeAsString) -> str:
-    expr = c.compile(n.expr)
-    return c.dialect.normalize_value_by_type(expr, n.expr_type or n.expr.type)
+
+class Compiler(Compiler):
+    @md
+    def compile_node(c: Compiler, n: NormalizeAsString) -> str:
+        expr = c.compile(n.expr)
+        return c.dialect.normalize_value_by_type(expr, n.expr_type or n.expr.type)
 
 
-@md
-def compile_node(c: Compiler, n: ApplyFuncAndNormalizeAsString) -> str:
-    expr = n.expr
-    expr_type = expr.type
+    @md
+    def compile_node(c: Compiler, n: ApplyFuncAndNormalizeAsString) -> str:
+        expr = n.expr
+        expr_type = expr.type
 
-    if isinstance(expr_type, Native_UUID):
-        # Normalize first, apply template after (for uuids)
-        # Needed because min/max(uuid) fails in postgresql
-        expr = NormalizeAsString(expr, expr_type)
-        if n.apply_func is not None:
-            expr = n.apply_func(expr)  # Apply template using Python's string formatting
+        if isinstance(expr_type, Native_UUID):
+            # Normalize first, apply template after (for uuids)
+            # Needed because min/max(uuid) fails in postgresql
+            expr = NormalizeAsString(expr, expr_type)
+            if n.apply_func is not None:
+                expr = n.apply_func(expr)  # Apply template using Python's string formatting
 
-    else:
-        # Apply template before normalizing (for ints)
-        if n.apply_func is not None:
-            expr = n.apply_func(expr)  # Apply template using Python's string formatting
-        expr = NormalizeAsString(expr, expr_type)
+        else:
+            # Apply template before normalizing (for ints)
+            if n.apply_func is not None:
+                expr = n.apply_func(expr)  # Apply template using Python's string formatting
+            expr = NormalizeAsString(expr, expr_type)
 
-    return c.compile(expr)
+        return c.compile(expr)
 
 
-@md
-def compile_node(c: Compiler, n: Checksum) -> str:
-    if len(n.exprs) > 1:
-        exprs = [Code(f"coalesce({c.compile(expr)}, '<null>')") for expr in n.exprs]
-        # exprs = [c.compile(e) for e in exprs]
-        expr = Concat(exprs, "|")
-    else:
-        # No need to coalesce - safe to assume that key cannot be null
-        (expr,) = n.exprs
-    expr = c.compile(expr)
-    md5 = c.dialect.md5_as_int(expr)
-    return f"sum({md5})"
+    @md
+    def compile_node(c: Compiler, n: Checksum) -> str:
+        if len(n.exprs) > 1:
+            exprs = [Code(f"coalesce({c.compile(expr)}, '<null>')") for expr in n.exprs]
+            # exprs = [c.compile(e) for e in exprs]
+            expr = Concat(exprs, "|")
+        else:
+            # No need to coalesce - safe to assume that key cannot be null
+            (expr,) = n.exprs
+        expr = c.compile(expr)
+        md5 = c.dialect.md5_as_int(expr)
+        return f"sum({md5})"
